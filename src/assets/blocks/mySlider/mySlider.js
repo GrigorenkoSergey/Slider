97970d16072fd49ba16fea186f9a971a6c1a9ca6
GrigorenkoSergey <crazybuilder51@gmail.com>
2020-04-16 12:57:12 +0300

    реализовал простенькое MVC

--- src/assets/blocks/mySlider/mySlider.js
+++ src/assets/blocks/mySlider/mySlider.js
@@ -1,5 +1,7 @@
-class Slider {
+class Model {
     constructor(options = {}) {
+        this.event = new EventObserver();
+
         this.min = options.min || 0;
         this.max = options.max || 100;
         this.thumbLeftPos = options.thumbLeftPos || 0;
@@ -11,20 +13,83 @@ class Slider {
         this.hintAboveThumb = options.hintAboveThumb || false;
     }
 
+    update(eventType, data) {
+        console.log("updating model");
+        this.event.broadcast("changeModel", this);
+    }
 }
 
-Slider.prototype.Model = function() {
-this.recieveDataFromController = (data) => {
-    Object.assign(this, data);    
-}
-}
-Slider.prototype.Controller = function() {
+class Presenter {
+    constructor(model, view) {
+        this.event = new EventObserver();
+
+        this.event.addSubscriber("changeView", model);
+        model && model.event && model.event.addSubscriber("changeModel", this);
+
+        this.event.addSubscriber("changeModel", view);
+        view && view.event && view.event.addSubscriber("changeView", this);
+    }
+
+    update(eventType, data) {
+        if (eventType == "changeModel") {
+            let result = this.translate(data);
+            this.event.broadcast("changeModel", result);
+
+        } else if (eventType == "changeView") {
+            this.event.broadcast("changeView", this);
+        }
+    }
+
+    translate(data) {
+        console.log("translating data!");
+        return 3;
+    }
 
 }
 
-Slider.prototype.View = function() {
+class View {
+    constructor() {
+        this.event = new EventObserver();
+    }
 
+    update(eventType, data) {
+        console.log("updating View");
+    }
+
+    someLogic() {
+        console.log("make some logic!");
+        this.event.broadcast("changeView", null);
+    }
 }
 
-let slider = new Slider();
-console.log(slider);
+class EventObserver {
+    //every subscriber should have method "update(eventType, data)"
+    constructor() {
+        this.observers = {};
+    }
+
+    addSubscriber(eventType, obj) {
+        this.observers[eventType] = this.observers[eventType] || [];
+        this.observers[eventType].push(obj);
+    }
+
+    removeSubscriber(eventType, obj) {
+        if (!this.observers[eventType]) return;
+        this.observers[eventType] = this.observers[eventType].filter(subscriber => subscriber !== fn);
+    }
+
+    broadcast(eventType, data) {
+        if (!this.observers[eventType]) return;
+        this.observers[eventType].forEach(subscriber => subscriber && subscriber.update(eventType, data));
+    }
+}
+
+let slider = new Model();
+
+let view = new View();
+
+let presenter = new Presenter(slider, view);
+
+slider.update();
+console.log("\n");
+view.someLogic();
