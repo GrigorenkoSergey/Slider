16e6fe6db9501e3389028b76c48c04e23f30f467
GrigorenkoSergey <crazybuilder51@gmail.com>
2020-12-28 11:08:35 +0300

    немного исправил ф-цию createUA

--- src/assets/blocks/helpers/functions/create-ua.ts
+++ src/assets/blocks/helpers/functions/create-ua.ts
@@ -1,6 +1,5 @@
 export function createUA<T extends string>(arr: T[]): T[] {
   // createUnionArray Не придумал более описательного имени...
-  type arrType = typeof arr[number];
-  const a: arrType[] = arr.slice();
+  const a: T[] = arr;
   return a;
 }
